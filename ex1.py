ed5581de24e2df798365099d7d7870e23f8eece0
LimJY <lgrnsdk@naver.com>
2020-04-02 13:03:23 +0900

    오늘부터 시작

---
+++ ex1.py
@@ -0,0 +1,27 @@
+import time
+
+from selenium.webdriver import Chrome
+
+chromeDriver = 'C:\\seleniumtemp\\chromedriver.exe'
+
+driver = Chrome(chromeDriver)
+
+driver.get('https://login.11st.co.kr/auth/front/login.tmall')
+
+time.sleep(3)
+
+input_login = driver.find_element_by_id("loginName")
+input_login.send_keys("")
+
+input_pw = driver.find_element_by_id("passWord")
+input_pw.send_keys("")
+
+btn = driver.find_element_by_class_name("btn_Atype")
+
+time.sleep(3)
+
+btn.click()
+
+time.sleep(3)
+
+driver.quit()
